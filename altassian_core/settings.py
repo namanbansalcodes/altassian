fc93af64fc2202e1fe3a77d6448bb9506728c812
namanbansalcodes <email@example.com>
2026-03-11 01:06:18 +0000

    Foundation setup: models, serializers, viewsets, URL routing, authentication

--- altassian_core/settings.py
+++ altassian_core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'spaces',
     'pages',
-    'accounts.apps.AccountsConfig',
+    'accounts',
     'djangorestframework_simplejwt',
     'django_cors_headers',
     'django_filters',
